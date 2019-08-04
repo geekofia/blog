29b67e254d1bcb3708f519291af992513346e3a3
chankruze <chankruze@gmail.com>
2019-08-04 22:21:51 +0530

    Navbar Re-Designed
    Signed-off-by: chankruze <chankruze@gmail.com>

--- assets/js/index.js
+++ assets/js/index.js
@@ -17,30 +17,4 @@ function goBack() {
     window.history.back()
 }
 
-// Get the navbar
-let navbar = document.querySelector('.navbar');
-
-// Get the offset position of the navbar
-let sticky = navbar.offsetTop;
-
-// When the user scrolls the page, execute myFunction
-window.onscroll = () => {
-    if (window.pageYOffset >= sticky) {
-        navbar.classList.add("fixed-top");
-    } else {
-        navbar.classList.remove("fixed-top");
-    }
-};
-
-$('.button-search').click(function(){
-    $('.form-control').show(1000);
-    $('.button-search').hide(1000);
-    $('.button-close').show(500);
-});
-
-$('.button-close').click(function(){
-    $('.form-control').val('');
-    $('.form-control').hide(1000);
-    $('.button-close').hide(1000);
-    $('.button-search').show(500);
-});
+// $("#search-input").css("width", $("#search-form").width() - 39 +"px");
