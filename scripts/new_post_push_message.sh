d3f97aad60c1a18db9e49e42df5c6beeb1ede06b
chankruze <chankruze@gmail.com>
2019-09-27 09:53:09 +0530

    New-Post: Dummy Post
    Signed-off-by: chankruze <chankruze@gmail.com>

--- scripts/new_post_push_message.sh
+++ scripts/new_post_push_message.sh
@@ -10,7 +10,7 @@ else
         --request POST \
         --header "Content-Type: application/json; charset=utf-8" \
         --header "Authorization: Basic $1" \
-        --data-binary @create_notification.json \
+        --data-binary @./scripts/create_notification.json \
         https://onesignal.com/api/v1/notifications;
 fi
 echo "=====================================================";
